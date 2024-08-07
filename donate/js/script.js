0ddc0bf16731f547754bf892eb4916059518d2d2
hack3duasoro <155655800+uasoro@users.noreply.github.com>
2024-08-07 18:32:07 +0300

    Add files via upload

---
+++ donate/js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const videoFiles = [
+      "media/v3.mp4",
+    ];
+  
+    const randomIndex = Math.floor(Math.random() * videoFiles.length);
+    const randomVideoFile = videoFiles[randomIndex];
+  
+    const videoElement = document.querySelector(".background-video video");
+    const sourceElement = document.createElement("source");
+  
+    sourceElement.setAttribute("src", randomVideoFile);
+    sourceElement.setAttribute("type", "video/mp4");
+  
+    videoElement.appendChild(sourceElement);
+    videoElement.load();
+  });
